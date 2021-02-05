9b480b65166a401e8783e981137006ab43c04e2c
delan-group <77882003+delan-group@users.noreply.github.com>
2021-02-05 18:27:12 +0800

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -1,6 +1,8 @@
+yum update
 yum -y install epel-release
 yum install python-pip
 yum install python3
 pip3 install requests pysocks
 git clone https://github.com/Leeon123/CC-attack.git
 cd CC-attack
+python3 cc.py
